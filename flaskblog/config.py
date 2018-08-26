eaf5caacb5962d3ed0b3fc80a54b0fff93e219ae
Jiahao Xu <jiahao-xu@Jiahaos-MBP.fios-router.home>
2018-08-25 20:46:54 -0400

    re-structure project

---
+++ flaskblog/config.py
@@ -0,0 +1,8 @@
+class Config:
+    SECRET_KEY = '124efcb1da0ee039b56e2d08d3a8d6ec'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = 'your own gmail address'
+    MAIL_PASSWORD = 'your own password'
